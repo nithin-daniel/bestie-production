1be37958ea53f5070e25c24f169fefc14a5995e1
nithindaniel1 <nithindaniel2018@gmail.com>
2023-08-12 22:34:18 +0530

    changes in urls

--- mybesties/urls.py
+++ mybesties/urls.py
@@ -22,7 +22,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += staticfiles_urlpatterns()
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
