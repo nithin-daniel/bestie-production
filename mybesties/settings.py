7c9dad15b7aa6b07c173ffce031422662ee84854
nithindaniel1 <nithindaniel2018@gmail.com>
2023-08-12 20:58:44 +0530

    .

--- mybesties/settings.py
+++ mybesties/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8&ar91zxn23m5fn%ho_-g^$lkjg_1syo^x4b7lmu2h)whp72)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
