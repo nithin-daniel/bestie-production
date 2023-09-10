331abac52e11913fc50f6b0e10f159425e35b5bb
Nithin Daniel <66398646+nithindaniel1@users.noreply.github.com>
2023-09-10 17:00:56 +0530

    Update settings.py

--- mybesties/settings.py
+++ mybesties/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8&ar91zxn23m5fn%ho_-g^$lkjg_1syo^x4b7lmu2h)whp72)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','mibestie.com']
+ALLOWED_HOSTS = ['127.0.0.1','mibestie.com','mibesties.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
