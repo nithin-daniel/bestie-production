014bbb42b89b4df3c951eea8567d10312c27f027
nithindaniel1 <nithindaniel2018@gmail.com>
2023-09-24 23:35:30 +0530

    allowed host

--- mybesties/settings.py
+++ mybesties/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-8&ar91zxn23m5fn%ho_-g^$lkjg_1syo^x4b7lmu2h)whp72)0
 # # Application definition
 import socket
 
-if socket.gethostname() == "mibestie.com":
+if socket.gethostname() == "www.mibestie.com":
     DEBUG = False
     ALLOWED_HOSTS = ["www.mibestie.com",]
 else:
